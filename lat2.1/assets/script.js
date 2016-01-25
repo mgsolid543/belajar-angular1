b49564f439b15fce97dae738ef3c16cde0c1947a
Fajar Bukhaeri <fajarbukhaeri08@gmail.com>
2016-01-26 00:49:15 +0700

    menampilkan nama student dengan ng-repeat dan script yang dibuat di file script.js

---
+++ lat2.1/assets/script.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function student($scope){
+    $scope.names = [
+        {name: 'Fajar Bukhaeri', age : 21},
+        {name: 'Harizal', age : 33},
+        {name: 'Irpan Budiana', age : 35}
+    ];
+}
