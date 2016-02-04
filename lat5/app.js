0352b376e75b0d5eee4c218c89391943dbd0afe8
Fajar Bukhaeri <fajarbukhaeri08@gmail.com>
2016-02-04 13:34:51 +0700

    done.

--- lat5/app.js
+++ lat5/app.js
@@ -32,6 +32,31 @@
      // state tentang dan view bernama majemuk =================================
      .state('tentang', {
       url: '/tentang',
-      templateUrl: 'tentang.html'
-     })
+      views: {
+        '': { templateUrl: 'tentang.html'},
+        'KolomSatu@tentang': { 
+            template: 'UI-Router merupakan framework routing untuk AngularJS yang dibangun oleh tim AngularUI. UI-router memungkinkan kita untuk membuat navigasi yang lebih baik.'
+        },
+        'KolomDua@tentang': {
+            templateUrl: 'data-table.html',
+            controller: 'tentangCtrl'
+        }
+      }
+    })
+});
+
+appku.controller('tentangCtrl', function($scope) {
+ $scope.list = [
+  {
+   nama: 'Hp Android S4',
+   harga: 'Rp 2 juta'
+  },
+  {
+   nama: 'Hp Iphone6',
+   harga: 'Rp 6 juta'
+  },
+  {
+   nama: 'Tablet Ipad mini',
+   harga: 'Rp 5 juta'
+  }];
 });
